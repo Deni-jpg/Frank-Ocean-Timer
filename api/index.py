b18367aa4125ef80b0abf8f9a16414cd979fc281
deni-jpg <soparamine23@gmail.com>
2026-02-24 12:34:17 +0000

    Fix index.py.

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 # configuração dos álbuns
 ALBUMS = {
